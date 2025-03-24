4ce5c8ad113468a7d7432ac69c50a8893354cdcf
hafiz07 <hkkhan074@gmail.com>
2025-03-25 04:53:12 +0600

    nwe

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://opdrape.store/',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000' || 'https://opdrape.store',
   credentials: true
 }));
 
