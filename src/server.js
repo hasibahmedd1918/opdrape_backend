50be8a2584e09161dea017bac18147eda4345675
hafiz07 <hkkhan074@gmail.com>
2025-03-25 05:04:05 +0600

    new

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000' || 'https://opdrape.store', 
   credentials: true
 }));
 app.use(helmet());
